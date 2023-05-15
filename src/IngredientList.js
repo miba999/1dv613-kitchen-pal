3a911a245aaabe80aafcfeab849d63ae19015749
Miranda Bånnsgård <bannsgard3@gmail.com>
2023-05-15 14:33:34 +0200

    Update IngredientList with error handling

--- src/IngredientList.js
+++ src/IngredientList.js
@@ -7,19 +7,36 @@ const IngredientList = ({ path }) => {
   const ingredientCollectionRef = collection(db, path);
 
   const [ingredients, setIngredients] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null)
 
   useEffect(() => {
     const getIngredients = async () => {
-      const data = await getDocs(ingredientCollectionRef);
-      setIngredients(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      try {
+        const data = await getDocs(ingredientCollectionRef);
+        setIngredients(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
     };
     getIngredients();
   }, []);
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  }
 
   return <ul>
     {ingredients.map((ingredient) => {
       return (
         <Ingredient
+          key={ingredient.id}
           amount={ingredient.amount}
           unit={ingredient.unit}
           name={ingredient.name}
