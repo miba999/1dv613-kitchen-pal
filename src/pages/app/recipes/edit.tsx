b4f23abcfdb1e4e3ef9d2b69c27db7b1f2440f1a
Miranda Bånnsgård <bannsgard3@gmail.com>
2025-05-16 16:42:50 +0200

    Add back button and extract RecipeEdit component

--- src/pages/app/recipes/edit.tsx
+++ src/pages/app/recipes/edit.tsx
@@ -1,16 +1,21 @@
-import * as React from 'react';
+import * as React from 'react'
+import { useParams } from 'react-router-dom'
+import RecipeEdit from '@/components/recipes/RecipeEdit'
+import BackButton from '@/components/recipes/form/BackButton'
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-interface IRecipeEditProps {
-  
-}
+const RecipeEditPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>()
 
-const RecipeEdit: React.FunctionComponent<IRecipeEditProps> = () => {
   return (
-    <div>
-      Edit Recipe
+    <div className="min-h-screen bg-muted py-6 px-4">
+      <div className="max-w-3xl mx-auto">
+        <div className="rounded-lg border bg-card shadow-md p-6 relative">
+          <BackButton to={`/recipes/${id}`} label="Tillbaka till receptet" />
+          <RecipeEdit />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default RecipeEdit;
+export default RecipeEditPage
