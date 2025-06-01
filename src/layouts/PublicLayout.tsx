e42e76d0ed17ad375473512ee270ad4d1b8ad16e
Miranda Bånnsgård <bannsgard3@gmail.com>
2025-06-01 18:36:10 +0200

    Fix styling

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,24 +1,12 @@
-import { Outlet, Link } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import Header from './Header'
-import { Button } from '@/components/ui/button'
 
 const PublicLayout = () => {
   return (
     <div className="min-h-screen flex flex-col bg-slate-50">
-      <Header
-        rightContent={
-          <div className="flex gap-2">
-            <Button variant="outline" asChild>
-              <Link to="/login">Logga in</Link>
-            </Button>
-            <Button asChild>
-              <Link to="/signup">Skapa konto</Link>
-            </Button>
-          </div>
-        }
-      />
+      <Header />
 
-      <main className="flex-grow container mx-auto px-4 py-8">
+      <main className="flex-grow bg-muted">
         <Outlet />
       </main>
     </div>
